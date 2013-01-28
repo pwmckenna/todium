c98e33070bf6a634b6ca51c73b70c0de672488dc
Patrick <pwmckenna@gmail.com>
2013-01-28 23:59:33 +0000

    adding the yeoman build files to gh-pages

--- scripts/view/f999c36f.stats.js
+++ scripts/view/f999c36f.stats.js
@@ -28,7 +28,7 @@ define([
             _.each(transferredDays, function(amount) {
                 total = amount;
             });
-            
+            console.log('onValue', 'total', total)
             this.series.append(new Date().getTime(), total);
         },
         onUser: function() {
@@ -45,8 +45,8 @@ define([
             var goal = new Smoothie.TimeSeries();
             goal.append(new Date().getTime(), 100000000000000000)
 
-            chart.addTimeSeries(this.series, { strokeStyle:'rgb(0, 255, 0)', fillStyle: 'rgba(255, 255, 255, 0.8)', lineWidth: 5 });
-            chart.addTimeSeries(goal, { fillStyle: 'rgba(255, 255, 255, 0.2)', lineWidth: 5 });
+            chart.addTimeSeries(this.series, { strokeStyle:'rgb(0, 255, 0)', fillStyle: 'rgba(255, 255, 255, 0.2)', lineWidth: 5 });
+            chart.addTimeSeries(goal, { fillStyle: 'rgba(255, 255, 255, 0.1)', lineWidth: 5 });
             chart.streamTo(this.$('.chart')[0], 1000);
 
             return this;
