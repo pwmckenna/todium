ccd906e18144e99a9bf4fa3a1a8630043a7a228e
Patrick <pwmckenna@gmail.com>
2013-01-29 01:02:08 +0000

    adding the yeoman build files to gh-pages

--- scripts/view/8894e17a.stats.js
+++ scripts/view/8894e17a.stats.js
@@ -41,12 +41,14 @@ define([
         render: function() {
             this.$el.html(this.template());
 
-            var chart = new Smoothie.SmoothieChart({ millisPerPixel: 100, grid: { strokeStyle: '#555555', lineWidth: 1, millisPerLine: 10000, verticalSections: 10 }});
+            var chart = new Smoothie.SmoothieChart({ millisPerPixel: 100, grid: { strokeStyle: '#555555', lineWidth: 1, millisPerLine: 10000, verticalSections: 4 }});
             var goal = new Smoothie.TimeSeries();
             goal.append(new Date().getTime(), 100000000000000000)
-
+            var zero = new Smoothie.TimeSeries();
+            zero.append(new Date().getTime(), 0);
             chart.addTimeSeries(this.series, { strokeStyle:'rgb(0, 255, 0)', fillStyle: 'rgba(255, 255, 255, 0.2)', lineWidth: 5 });
-            chart.addTimeSeries(goal, { fillStyle: 'rgba(255, 255, 255, 0.1)', lineWidth: 5 });
+            chart.addTimeSeries(goal, { lineWidth: 0 });
+            chart.addTimeSeries(zero, { lineWidth: 0 });
             chart.streamTo(this.$('.chart')[0], 1000);
 
             return this;
