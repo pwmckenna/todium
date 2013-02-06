b58ac262884b2bb1ca3fe2e6456d0b60888ccad7
Patrick <pwmckenna@gmail.com>
2013-02-06 00:55:30 +0000

    adding the yeoman build files to gh-pages

--- scripts/view/051f3944.stats.js
+++ scripts/view/051f3944.stats.js
@@ -17,6 +17,7 @@ define([
             this.completedSeries = new Smoothie.TimeSeries();
             this.startedSeries = new Smoothie.TimeSeries();
             this.stoppedSeries = new Smoothie.TimeSeries();
+            this.transferredSeries = new Smoothie.TimeSeries();
             
             this.model.on('change:user', this.onUser, this);
             this.model.firebase.child('stats').on('value', this.onValue, this);
@@ -24,7 +25,8 @@ define([
             setTimeout(_.bind(this.resizeCanvas, this));
         },
         resizeCanvas: function() {
-            this.$('.chart')[0].width = this.$el.parent().innerWidth();
+            this.$('.transferredChart')[0].width = this.$el.parent().innerWidth();
+            this.$('.trackerEventChart')[0].width = this.$el.parent().innerWidth();
         },
         onValue: function(valueSnapshot) {
             console.log('onValue');
@@ -38,7 +40,7 @@ define([
                 totalTransferred += val;
             });
             this.$('.transferred').text(bytesToSize(totalTransferred));
-            console.log(completed, started, stopped);
+            this.transferredSeries.append(new Date().getTime(), totalTransferred);
             this.completedSeries.append(new Date().getTime(), completed);
             this.startedSeries.append(new Date().getTime(), started);
             this.stoppedSeries.append(new Date().getTime(), stopped);
@@ -50,46 +52,71 @@ define([
                 this.$el.show();
             }
         },
-        render: function() {
-            console.log('render');
-            this.$el.html(this.template({
-                transferred: 0
-            }));
-
+        renderTrackerEventGraph : function() {
             var chart = new Smoothie.SmoothieChart({ 
                 millisPerPixel: 100, 
                 grid: { 
                     strokeStyle: 'rgba(245, 245, 245, 0.2)',
-                    fillStyle:'rgba(255, 255, 255, 0.1)',
+                    fillStyle:'rgba(255, 255, 255, 0.0)',
                     lineWidth: 1,
                     millisPerLine: 10000, 
                     verticalSections: 4 
                 }
             });
             var goal = new Smoothie.TimeSeries();
-            goal.append(new Date().getTime(), 100000000000000000)
+            goal.append(new Date().getTime(), 100000000000000000);
             var zero = new Smoothie.TimeSeries();
             zero.append(new Date().getTime(), 0);
 
             chart.addTimeSeries(this.completedSeries, { 
                 strokeStyle:'#3A87AD', 
-                fillStyle:'rgba(255, 255, 255, 0.1)', 
+                fillStyle:'rgba(255, 255, 255, 0.0)', 
                 lineWidth: 3 
             });
             chart.addTimeSeries(this.startedSeries, { 
                 strokeStyle:'green', 
-                fillStyle:'rgba(150, 255, 150, 0.1)', 
+                fillStyle:'rgba(150, 255, 150, 0.0)', 
                 lineWidth: 3 
             });
             chart.addTimeSeries(this.stoppedSeries, {
                 strokeStyle:'red', 
-                fillStyle:'rgba(255, 255, 255, 0.1)', 
+                fillStyle:'rgba(255, 255, 255, 0.0)', 
                 lineWidth: 3 
             });
             chart.addTimeSeries(zero, { lineWidth: 0 });
 
-            chart.streamTo(this.$('.chart')[0], 1000);
+            chart.streamTo(this.$('.trackerEventChart')[0], 1000);
+        },
+        renderTransferredGraph: function() {
+            var chart = new Smoothie.SmoothieChart({ 
+                millisPerPixel: 100, 
+                grid: { 
+                    strokeStyle: 'rgba(245, 245, 245, 0.2)',
+                    fillStyle:'rgba(255, 255, 255, 0.0)',
+                    lineWidth: 1,
+                    millisPerLine: 10000, 
+                    verticalSections: 4 
+                }
+            });
+            var zero = new Smoothie.TimeSeries();
+            zero.append(new Date().getTime(), 0);
 
+            chart.addTimeSeries(this.transferredSeries, {
+                strokeStyle:'#3A87AD',
+                fillStyle:'rgba(255, 255, 255, 0.0)',
+                lineWidth: 3
+            });
+            chart.addTimeSeries(zero, { lineWidth: 0 });
+
+            chart.streamTo(this.$('.transferredChart')[0], 1000);
+        },
+        render: function() {
+            console.log('render');
+            this.$el.html(this.template({
+                transferred: 0
+            }));
+            this.renderTrackerEventGraph();
+            this.renderTransferredGraph();
             return this;
         }
     });
