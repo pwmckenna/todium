d94b8f48690bb3952054cb5ea5dd94d4ce90c577
Patrick Williams <pwmckenna@gmail.com>
2013-03-14 15:23:57 -0700

    tying the radio buttons to the campaign sub views

--- app/scripts/view/campaign.js
+++ app/scripts/view/campaign.js
@@ -7,6 +7,9 @@ define([
     'use strict';
 
     var CampaignView = View.extend({
+        events: {
+            'click .btn.owners, .btn.api, .btn.trackers': 'toggleViews'
+        },
         initialize: function () {
             this.template = _.template($('#campaign_template').html());
             this.views = {};
@@ -20,6 +23,26 @@ define([
                 model: this.model
             });
             this.model.child('name').on('value', this.render, this);
+        },
+        toggleViews: function () {
+            var _this = this;
+            setTimeout(function () {
+                if(_this.$('.btn.trackers').hasClass('active')) {
+                    _this.trackersView.$el.show();
+                } else {
+                    _this.trackersView.$el.hide();
+                }
+                if(_this.$('.btn.api').hasClass('active')) {
+                    _this.apiView.$el.show();
+                } else {
+                    _this.apiView.$el.hide();
+                }
+                if(_this.$('.btn.owners').hasClass('active')) {
+                    _this.ownersView.$el.show();
+                } else {
+                    _this.ownersView.$el.hide();
+                }
+            });
         },
         destroy: function () {
             this.model.child('name').off('value', this.render, this);
@@ -38,9 +61,10 @@ define([
             this.$el.html(this.template({
                 name: name,
             }));
-            this.assign(this.apiView, '.api');
-            this.assign(this.ownersView, '.owners');
-            this.assign(this.trackersView, '.trackers');
+            this.assign(this.apiView, '.well.api');
+            this.assign(this.ownersView, '.well.owners');
+            this.assign(this.trackersView, '.well.trackers');
+            this.toggleViews();
             return this;
         }
     });
