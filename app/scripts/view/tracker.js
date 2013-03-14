13b224c12e06bcf76b2464152ad5db07a28949d7
Patrick Williams <pwmckenna@gmail.com>
2013-03-14 15:29:35 -0700

    ripping out the c+p functionality

--- app/scripts/view/tracker.js
+++ app/scripts/view/tracker.js
@@ -3,16 +3,6 @@ define([
 ], function (View) {
     'use strict';
 
-    var clip = new ZeroClipboard.Client();
-    clip.setHandCursor(true);
-    clip.addEventListener('complete', function () {
-        var linkElem = $(clip.domElement).prev();
-        linkElem.attr('value', ' Copied to clipboard!');
-        setTimeout(function () {
-            linkElem.attr('value', linkElem.attr('url'));
-        }, 1000);
-    });
-
     // var bytesToSize = function (bytes) {
     //     var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB'];
     //     if (bytes === 0) {
@@ -24,9 +14,6 @@ define([
 
 
     var TrackerView = View.extend({
-        events: {
-            'mouseover .copy': 'onCopy'
-        },
         initialize: function () {
             this.url = '...';
             this.template = _.template($('#tracker_template').html());
@@ -39,19 +26,6 @@ define([
             console.log('onValue', valueSnapshot.val());
             this.val = valueSnapshot.val();
             this.render();
-        },
-        onCopy: function () {
-            console.log('onCopy', this.url);
-            clip.setText(this.url);
-
-            var elem = this.$('.copy')[0];
-            if (clip.div) {
-                clip.receiveEvent('mouseout', null);
-                clip.reposition(elem);
-            } else {
-                clip.glue(elem);
-            }
-            clip.receiveEvent('mouseover', null);
         },
         render: function () {
             this.$el.html(this.template({
