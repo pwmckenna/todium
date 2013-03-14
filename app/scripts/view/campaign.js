17de793c95e868efacc4f12448e81b5dd3c53710
Patrick Williams <pwmckenna@gmail.com>
2013-03-14 10:06:24 -0700

    moving the campaign torrent trackers view into a seperate file

--- app/scripts/view/campaign.js
+++ app/scripts/view/campaign.js
@@ -1,15 +1,12 @@
 define([
     './view',
-    './tracker',
     './api',
-    './owners'
-], function (View, TrackerView, ApiView, OwnersView) {
+    './owners',
+    './trackers'
+], function (View, ApiView, OwnersView, TrackersView) {
     'use strict';
 
     var CampaignView = View.extend({
-        events: {
-            'click .addTracker': 'onAddTracker'
-        },
         initialize: function () {
             this.template = _.template($('#campaign_template').html());
             this.views = {};
@@ -19,88 +16,33 @@ define([
             this.ownersView = new OwnersView({
                 model: this.model
             });
-            setTimeout(_.bind(function () {
-                this.model.child('trackers').on('child_added', this.onTrackerAdded, this);
-                this.model.child('trackers').on('child_removed', this.onTrackerRemoved, this);
-                this.model.on('value', this.render, this);
-            }, this));
+            this.trackersView = new TrackersView({
+                model: this.model
+            });
+            this.model.child('name').on('value', this.render, this);
         },
         destroy: function () {
-            this.model.child('trackers').off('child_added', this.onTrackerAdded, this);
-            this.model.child('trackers').off('child_removed', this.onTrackerRemoved, this);
-            this.model.off('value', this.render, this);
+            this.model.child('name').off('value', this.render, this);
             this.apiView.destroy();
             this.apiView.remove();
-            _.each(this.views, function (view) {
-                view.destroy();
-                view.remove();
-            });
-            this.views = {};
-        },
-        onTrackerAdded: function (dataSnapshot) {
-            console.log('onTrackerAdded', dataSnapshot.val());
-            var trackerName = dataSnapshot.val();
-            var tracker = this.model.parent().parent().child('trackers').child(trackerName);
-            var view = new TrackerView({
-                model: tracker
-            });
-            this.views[trackerName] = view;
-            this.$('.trackers').append(view.$el);
-        },
-        onTrackerRemoved: function (dataSnapshot) {
-            console.log('onTrackerRemoved', dataSnapshot.val());
-            var trackerName = dataSnapshot.val();
-            var view = this.views[trackerName];
-            view.remove();
-            delete this.views[trackerName];
-        },
-        onAddTracker: function () {
-            var button = this.$('.btn');
-            if (button.hasClass('disabled')) {
-                return;
-            }
-            button.addClass('disabled');
-            setTimeout(function () {
-                button.removeClass('disabled');
-            }, 3000);
-
-            var torrentLink = this.$('.createTorrentLink').val();
-            if (!torrentLink) {
-                return;
-            }
-            this.$('.createTorrentLink').val('');
-
-            this.model.child('secret').once('value', function (secretSnapshot) {
-                var secret = secretSnapshot.val();
-                $.getJSON('http://api.todium.com', {
-                    id: this.model.name(),
-                    secret: secret,
-                    src: torrentLink
-                }).then(function (url) {
-                    console.log(url);
-                });
-            }, this);
+            this.ownersView.destroy();
+            this.ownersView.remove();
+            this.trackersView.destroy();
+            this.trackersView.remove();
         },
         render: function () {
             var trackers = this.$('.trackers').children().detach();
 
             var name = '';
-            var id = this.model.name();
-            var secret = '';
-            this.model.once('value', function (valueSnapshot) {
-                var val = valueSnapshot.val();
-                name = val.name;
-                secret = val.secret;
+            this.model.child('name').once('value', function (valueSnapshot) {
+                name = valueSnapshot.val();
             });
-
             this.$el.html(this.template({
                 name: name,
-                id: id,
-                secret: secret
             }));
-            this.$('.trackers').append(trackers);
             this.assign(this.apiView, '.api');
             this.assign(this.ownersView, '.owners');
+            this.assign(this.trackersView, '.trackers');
             return this;
         }
     });
---
+++ app/scripts/view/trackers.js
@@ -0,0 +1,88 @@
+define([
+    './view',
+    './tracker'
+], function (View, TrackerView) {
+    'use strict';
+
+    var TrackersView = View.extend({
+        events: {
+            'click .addTracker': 'onAddTracker'
+        },
+        initialize: function () {
+            this.template = _.template($('#trackers_template').html());
+            this.views = {};
+            this.model.child('trackers').on('child_added', this.onTrackerAdded, this);
+            this.model.child('trackers').on('child_removed', this.onTrackerRemoved, this);
+        },
+        destroy: function () {
+            this.model.child('trackers').off('child_added', this.onTrackerAdded, this);
+            this.model.child('trackers').off('child_removed', this.onTrackerRemoved, this);
+            _.each(this.views, function (view) {
+                view.destroy();
+                view.remove();
+            });
+            this.views = {};
+        },
+        onTrackerAdded: function (dataSnapshot) {
+            var _this = this;
+            setTimeout(function () {
+                console.log('onTrackerAdded', dataSnapshot.val());
+                var trackerName = dataSnapshot.val();
+                var tracker = _this.model.parent().parent().child('trackers').child(trackerName);
+                var view = new TrackerView({
+                    model: tracker
+                });
+                _this.views[trackerName] = view;
+                _this.$('.trackers').append(view.$el);
+            })
+        },
+        onTrackerRemoved: function (dataSnapshot) {
+            console.log('onTrackerRemoved', dataSnapshot.val());
+            var trackerName = dataSnapshot.val();
+            var view = this.views[trackerName];
+            view.remove();
+            delete this.views[trackerName];
+        },
+        onAddTracker: function () {
+            var button = this.$('.btn');
+            if (button.hasClass('disabled')) {
+                return;
+            }
+            button.addClass('disabled');
+            setTimeout(function () {
+                button.removeClass('disabled');
+            }, 3000);
+
+            var torrentLink = this.$('.createTorrentLink').val();
+            if (!torrentLink) {
+                return;
+            }
+            this.$('.createTorrentLink').val('');
+
+            this.model.child('secret').once('value', function (secretSnapshot) {
+                var secret = secretSnapshot.val();
+                $.getJSON('http://api.todium.com', {
+                    id: this.model.name(),
+                    secret: secret,
+                    src: torrentLink
+                }).then(function (url) {
+                    console.log(url);
+                });
+            }, this);
+        },
+        render: function () {
+            var trackers = this.$('.trackers').children().detach();
+
+            var name = '';
+            this.model.child('name').once('value', function (valueSnapshot) {
+                name = valueSnapshot.val();
+            });
+            this.$el.html(this.template({
+                name: name,
+            }));
+            this.$('.trackers').append(trackers);
+            return this;
+        }
+    });
+    return TrackersView;
+});
