fd5914302c1ce81be014989daa3ddea16207686d
Patrick Williams <pwmckenna@gmail.com>
2013-03-18 15:43:12 -0700

    new rule, no partial code with firebase callbacks. disaster

--- app/scripts/view/stat.js
+++ app/scripts/view/stat.js
@@ -1,7 +1,7 @@
 define([
     './view',
     'chart'
-], function (View, Chart) {
+], function (View) {
     'use strict';
     var StatView = View.extend({
         initialize: function () {
@@ -39,35 +39,6 @@ define([
             });
             console.log('charting data', value);
 
-            var canvas = this.$('canvas');
-            var ctx = canvas.get(0).getContext('2d');
-            var chart = new Chart(ctx);
-
-            var data = {
-                labels : [],
-                datasets : [
-                    {
-                        fillColor : 'rgba(220, 220, 220, 0.5)',
-                        strokeColor : 'rgba(220, 220, 220, 1)',
-                        pointColor : 'rgba(220, 220, 220, 1)',
-                        pointStrokeColor : '#fff',
-                        data : [65, 59, 90, 81, 56, 55, 40]
-                    },
-                    {
-                        fillColor : 'rgba(151, 187, 205, 0.5)',
-                        strokeColor : 'rgba(151, 187, 205, 1)',
-                        pointColor : 'rgba(151, 187, 205, 1)',
-                        pointStrokeColor : '#fff',
-                        data : [28, 48, 40, 19, 96, 27, 100]
-                    }
-                ]
-            };
-            var options = {
-                scaleShowLabels: false,
-                scaleOverlay: false,
-                pointDot: false
-            };
-            chart.Line(data, options);
             return this;
         }
     });
