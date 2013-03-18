40f6d53cd7955186309db1c4786636aadbda9e2b
Patrick Williams <pwmckenna@gmail.com>
2013-03-18 14:55:42 -0700

    fixing a graphing bug that was blowing up all firebase value callbacks

--- app/scripts/view/stat.js
+++ app/scripts/view/stat.js
@@ -37,36 +37,37 @@ define([
             this.model.child('stats').once('value', function (valueSnapshot) {
                 value = valueSnapshot.val();
             });
+            console.log('charting data', value);
 
-            var canvas = this.$('canvas');
-            var ctx = canvas.get(0).getContext('2d');
-            var chart = new Chart(ctx);
+            // var canvas = this.$('canvas');
+            // var ctx = canvas.get(0).getContext('2d');
+            // var chart = new Chart(ctx);
 
-            var data = {
-                // labels : ["January","February","March","April","May","June","July"],
-                // datasets : [
-                //     {
-                //         fillColor : "rgba(220,220,220,0.5)",
-                //         strokeColor : "rgba(220,220,220,1)",
-                //         pointColor : "rgba(220,220,220,1)",
-                //         pointStrokeColor : "#fff",
-                //         data : [65,59,90,81,56,55,40]
-                //     },
-                //     {
-                //         fillColor : "rgba(151,187,205,0.5)",
-                //         strokeColor : "rgba(151,187,205,1)",
-                //         pointColor : "rgba(151,187,205,1)",
-                //         pointStrokeColor : "#fff",
-                //         data : [28,48,40,19,96,27,100]
-                //     }
-                // ]
-            };
-            var options = {
-                scaleShowLabels: false,
-                scaleOverlay: false,
-                pointDot: false
-            };
-            chart.Line(data, options);
+            // var data = {
+            //     // labels : ["January","February","March","April","May","June","July"],
+            //     // datasets : [
+            //     //     {
+            //     //         fillColor : "rgba(220,220,220,0.5)",
+            //     //         strokeColor : "rgba(220,220,220,1)",
+            //     //         pointColor : "rgba(220,220,220,1)",
+            //     //         pointStrokeColor : "#fff",
+            //     //         data : [65,59,90,81,56,55,40]
+            //     //     },
+            //     //     {
+            //     //         fillColor : "rgba(151,187,205,0.5)",
+            //     //         strokeColor : "rgba(151,187,205,1)",
+            //     //         pointColor : "rgba(151,187,205,1)",
+            //     //         pointStrokeColor : "#fff",
+            //     //         data : [28,48,40,19,96,27,100]
+            //     //     }
+            //     // ]
+            // };
+            // var options = {
+            //     scaleShowLabels: false,
+            //     scaleOverlay: false,
+            //     pointDot: false
+            // };
+            // chart.Line(data, options);
             return this;
         }
     });
